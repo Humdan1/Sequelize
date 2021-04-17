0ca65457dcd3f298f76f5c334bfbfb541a46d185
Humdan1 <36046285+Humdan1@users.noreply.github.com>
2021-04-16 21:01:06 -0400

    2

--- public/script.js
+++ public/script.js
@@ -1,101 +1,105 @@
-
-/*async function dataHandler() {
-  const endpoint = '/api/meals';
-  console.log(endpoint);
-  const request = await fetch(endpoint);
-  const meals = await request.json();
-  return meals;
-
-}
-
-// This function loads the dining halls into the table
-
-
-// Once the request is successfully retrieved, pass the data into our table function
-dataHandler().then(meals => {
-  console.log(meals);
-
- // for (i = 0; i < dininghalls.data.length; i += 1) {
-   // loadTableData(dininghalls.data[i]);
-//  }
-})*/
-
-window.onload = function () {
-
-
-
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+async function dataHandler() {
+	// Retrieve dining, meals and macros endpoints
+	const endpoint1 = '/api/dining';
+	const request1 = await fetch(endpoint1);
+	const dininghalls = await request1.json();
+  
+	const endpoint2 = '/api/meals';
+	const request2 = await fetch(endpoint2);
+	const meals = await request2.json();
+  
+	const endpoint3 = '/api/macros';
+	const request3 = await fetch(endpoint3);
+	const macros = await request3.json();
+  
+	console.log(dininghalls);
+	console.log(meals);
+	console.log(macros);
+  
+	// This function extracts the datapoints we need from the data collected above
+	function getData(mealss, macross, macro) {
+	  data = [];
+	  for (i = 0; i < 46; i += 1) {
+		data.push({ label: mealss[i].meal_name, y: eval(`macross[i].${macro}`)});
+	  }
+	  return data;
+	}
+  
+	// Create chart variable
+	const chart = new CanvasJS.Chart('chartContainer', {
+	  animationEnabled: true,
+	  title: {
+		text: 'Meal Macros'
+	  },
+	  axisX: {
+		labelFontSize: 10,
+		interval: 1
+	  },
+	  axisY: {
+		labelFontSize: 10
+	  },
+	  toolTip: {
+		shared: true
+	  },
+	  legend: {
+		cursor: 'pointer',
+		itemclick: toggleDataSeries
+	  },
+  
+	  dataPointWidth: 10,
+  
+	  data: [{
+		type: 'stackedBar',
+		name: 'Calories',
+		showInLegend: 'true',
+		dataPoints: getData(meals, macros, 'calories')
+	  },
+	  {
+		type: 'stackedBar',
+		name: 'Carbs',
+		showInLegend: 'true',
+		dataPoints: getData(meals, macros, 'carbs')
+	  },
+	  {
+		type: 'stackedBar',
+		name: 'Cholesterol',
+		showInLegend: 'true',
+		dataPoints: getData(meals, macros, 'cholesterol')
+	  },
+	  {
+		type: 'stackedBar',
+		name: 'Fat',
+		showInLegend: 'true',
+		dataPoints: getData(meals, macros, 'fat')
+	  },
+	  {
+		type: 'stackedBar',
+		name: 'Protein',
+		showInLegend: 'true',
+		dataPoints: getData(meals, macros, 'protein')
+	  },
+	  {
+		type: 'stackedBar',
+		name: 'Sodium',
+		showInLegend: 'true',
+		dataPoints: getData(meals, macros, 'sodium')
+	  }
+	  ]
+  
+	});
+  
+	chart.render();
+  
+	// Render chart
+	function toggleDataSeries(e) {
+	  if (typeof (e.dataSeries.visible) === 'undefined' || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	  } else {
+		e.dataSeries.visible = true;
+	  }
+	  chart.render();
+	}
   }
   
-    console.log(getRandomInt(3));
-    // expected output: 0, 1 or 2
-    
-    //console.log(getRandomInt(1));
-    // expected output: 0
-    
-    //console.log(Math.random());
-    // expected output: a number from 0 to <1
-  
-
-const chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	theme: "light2", //"light1", "dark1", "dark2"
-	title:{
-		text: "Meals and their Macros"             
-	},
-	axisY:{
-		interval: 10,
-		suffix: "%"
-	},
-	toolTip:{
-		shared: true
-	},
-	data:[{
-		type: "stackedBar100",
-		toolTipContent: "{label}<br><b>{name}:</b> {y} (#percent%)",
-		showInLegend: true, 
-		name: "April",
-		dataPoints: [
-			{ y: 600, label: "Water Filter" },
-			{ y: 400, label: "Modern Chair" },
-			{ y: 120, label: "VOIP Phone" },
-			{ y: 250, label: "Microwave" },
-			{ y: 120, label: "Water Filter" },
-			{ y: 374, label: "Expresso Machine" },
-			{ y: 350, label: "Lobby Chair" }
-		]
-		},
-		{
-			type: "stackedBar100",
-			toolTipContent: "<b>{name}:</b> {y} (#percent%)",
-			showInLegend: true, 
-			name: "May",
-			dataPoints: [
-				{ y: 400, label: "Water Filter" },
-				{ y: 500, label: "Modern Chair" },
-				{ y: 220, label: "VOIP Phone" },
-				{ y: 350, label: "Microwave" },
-				{ y: 220, label: "Water Filter" },
-				{ y: 474, label: "Expresso Machine" },
-				{ y: 450, label: "Lobby Chair" }
-			]
-		}, 
-		{
-			type: "stackedBar100",
-			toolTipContent: "<b>{name}:</b> {y} (#percent%)",
-			showInLegend: true, 
-			name: "June",
-			dataPoints: [
-				{ y: 300, label: "Water Filter" },
-				{ y: 610, label: "Modern Chair" },
-				{ y: 215, label: "VOIP Phone" },
-				{ y: 221, label: "Microwave" },
-				{ y: 75, label: "Water Filter" },
-				{ y: 310, label: "Expresso Machine" },
-				{ y: 340, label: "Lobby Chair" }
-			]
-	}]
-});
-chart.render();
-}
+  // Load our page
+  dataHandler();
